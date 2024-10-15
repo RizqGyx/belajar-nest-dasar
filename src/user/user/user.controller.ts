8e350828a4ec67d250944e1055ab9abe071a4686
Rizki <mhdrizky32@gmail.com>
2024-10-15 14:27:27 +0700

    Fixing[feat]: user Controller with reccoment HTTP req

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('/api/v1/user')
@@ -10,11 +10,25 @@ export class UserController {
 
   @Get()
   getUsers(): string {
-    return 'Ini Get';
+    return 'Ini Get Biasa';
   }
 
+  @Get('/welcome')
+  sayWelcome(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+    @Query('nim') nim: number,
+  ): string {
+    return `Welcome ${firstName + ' ' + lastName || 'Guest'} with Nim ${nim}!`;
+  }
+
+  //   @Get('/:id')
+  //   getUserById(@Req() request: Request): string {
+  //     return `Ini Get dengan ID ${request.params.id}`;
+  //   }
+
   @Get('/:id')
-  getUserById(@Req() request: Request): string {
-    return `Ini Get dengan ID ${request.params.id}`;
+  getUserById(@Param('id') id: string): string {
+    return `Ini Get dengan ID ${id}`;
   }
 }
